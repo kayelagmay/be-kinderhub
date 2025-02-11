2c25733b50c9f0df6f300995263dff42f305c7c9
Kaye Lagmay <kayelagmay@icloud.com>
2025-02-11 18:56:07 +0000

    Streamline routes in index.js and amended .env file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const userRoutes = require('./users');
+const eventRoutes = require('./events');
+const bookingRoutes = require('./bookings');
+const childrenRoutes = require('./children');
 
+router.use('/users', userRoutes);
+router.use('/events', eventRoutes);
+router.use('/bookings', bookingRoutes);
+router.use('/children', childrenRoutes);
+
+// Optional root route to check if API is running
 router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to KinderHub API '});
+  res.json({ message: 'Welcome to the KinderHub API' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/index');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const routes = require('./routes/index');
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
